f623b984d4c935559ff9fcd0f8c7a1b6b241a4b1
shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
2024-01-19 14:57:06 +0530

    update scrollFactorAtTop

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function preventDefault(e) {
 	let currentlyAt = document.documentElement.scrollTop
 
 	// calculate scroll factor
-	const scrollFactorAtTop = 1
+	const scrollFactorAtTop = 1.2
 	const scrollFactorAtBottom = 0.1
 	const rateOfScrollFactorChange = 2
 
